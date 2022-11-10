b2cca4cdf0091b00630c01af6e6cec7043b88f38
cyrian simard <cyriansimard@gmail.com>
2022-11-10 15:02:57 +0100

    feat(home): change foreach to for

--- scripts/model/ModelRecipe.js
+++ scripts/model/ModelRecipe.js
@@ -29,38 +29,54 @@ class ModelRecipe extends Publisher {
 
         if (this.search !== '') {
             let filteredDataRecipe = [];
-            this.filteredRecipes.forEach((recipe) => {
-                if (recipe.name.toLowerCase().includes(this.search.toLowerCase())) {
-                    filteredDataRecipe.push(recipe);
+            for (let i = 0; i < this.filteredRecipes.length ; i++) {
+                if (this.filteredRecipes[i].name.toLowerCase().includes(this.search.toLowerCase())) {
+                    filteredDataRecipe.push(this.filteredRecipes[i]);
                 }
-            })
+            }
             this.filteredRecipes = filteredDataRecipe;
         }
 
         if (this.tags.ingredients !== undefined) {
-            this.tags.ingredients.forEach((tagIngredient) => {
+            for (let i = 0; i < this.tags.ingredients.length; i++) {
                 let filteredDataRecipe = [];
-                this.filteredRecipes.forEach((recipe) => {
-                    recipe.ingredients.forEach((ingredient) => {
-                        if (ingredient.ingredient === tagIngredient) {
-                            filteredDataRecipe.push(recipe)
+                for (let a = 0; a < this.filteredRecipes.length; a++) {
+                    let ingredients = this.filteredRecipes[a].ingredients;
+                    for (let b = 0; b < ingredients.length; b++) {
+                        if (ingredients[b].ingredient === this.tags.ingredients[i]) {
+                            filteredDataRecipe.push(this.filteredRecipes[a]);
                         }
-                    })
-                })
+                    }
+                }
                 this.filteredRecipes = filteredDataRecipe;
-            })
+            }
         }
 
         if (this.tags.appliances !== undefined) {
-            this.tags.appliances.forEach((tagAppliance) => {
+            for (let i = 0; i < this.tags.appliances.length; i++) {
                 let filteredRecipes = [];
-                this.filteredRecipes.forEach((filteredRecipe) => {
-                    if (filteredRecipe.appliance === tagAppliance) {
-                        filteredRecipes.push(filteredRecipe);
+                for (let a = 0; a < this.filteredRecipes.length; a++) {
+                    if (this.filteredRecipes[a].appliance === this.tags.appliances[i]) {
+                        filteredRecipes.push(this.filteredRecipes[a]);
                     }
-                })
+                }
                 this.filteredRecipes = filteredRecipes;
-            })
+            }
+        }
+
+        if (this.tags.ustensils !== undefined) {
+            for (let i = 0; i < this.tags.ustensils.length; i++) {
+                let filteredRecipes = [];
+                for (let a = 0; a < this.filteredRecipes.length; a++) {
+                    let filteredRecipe = this.filteredRecipes[a].ustensils;
+                    for (let b = 0; b < filteredRecipe.length; b++) {
+                        if (filteredRecipe[b] === this.tags.ustensils[i]) {
+                            filteredRecipes.push(this.filteredRecipes[a]);
+                        }
+                    }
+                }
+                this.filteredRecipes = filteredRecipes;
+            }
         }
 
         if (this.tags.ustensils !== undefined) {
